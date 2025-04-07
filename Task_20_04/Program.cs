3b998a76f0d6203211a1f55884bcea886e5023f5
Roman <asidaleeva@gmail.com>
2025-04-07 19:58:32 +0300

    задания из 19 и 20 лекции решены

--- Task_20_04/Program.cs
+++ Task_20_04/Program.cs
@@ -2,6 +2,15 @@
 {
     internal class Program
     {
+        /* Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+    • Car
+    • Bike
+    • Bus
+    • Truck
+    • Motorcycle
+    Храните список транспортных средств (можно просто List<VehicleType>).
+    Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков).
+    Реализуйте поиск по типу и вывод информации*/
         static void Main(string[] args)
         {
             VehicleManager vehicleManager = new VehicleManager();
